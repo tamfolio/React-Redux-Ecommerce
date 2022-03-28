f8f71ff21c7ceb0e93be861b1e1084c7e0559c24
unknown <tamiloreolaniyi@gmail.com>
2022-03-28 20:23:03 +0100

    navbar complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
+import './Navbar.css'
 
 function Navbar() {
   return (
     <div>
-        
+        <nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm py-3">
+  <div class="container">
+    <a class="navbar-brand fw-bold fs-4" href="/">DarkMan Collection</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/">Product</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/">Contact</a>
+        </li>
+      </ul>
+      <div className="buttons">
+          <button className="btn">
+              <a href="/" className='btn btn-outline-dark'>
+                  <i className="fa fa-sign-in me-1"></i> Login
+              </a>
+              <a href="/" className='btn btn-outline-dark ms-2'>
+                  <i className="fa fa-user-plus me-1"></i> Register
+              </a>
+              <a href="/" className='btn btn-outline-dark ms-2'>
+                  <i className="fa fa-shopping-cart me-1"></i> Cart (0)
+              </a> 
+          </button>
+      </div>
+    </div>
+  </div>
+</nav>
     </div>
   )
 }
