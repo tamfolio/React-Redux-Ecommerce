ba8be878882aa2b182316c7bf7ae33916ba75b23
unknown <tamiloreolaniyi@gmail.com>
2022-03-28 21:30:23 +0100

    banner image and text

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,12 +5,11 @@ function Home() {
     <div className='hero'>
         <div class="card bg-dark text-white border-0">
         <img src="./assets/bg3.jpg" class="card-img" alt="Background" height='550px'/>
-        <div class="card-img-overlay">
+        <div class="card-img-overlay d-flex flex-column  justify-content-center align-items-center text-center">
             <div className="container">
-            <h5 class="card-title display-3">NEW SEASON ARRIVAL</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <h5 class="card-title display-3 fw-bolder mb-0">NEW SEASON ARRIVAL</h5>
+            <p class="card-text lead fs-2">CHECK OUT THE COLLECTIONS</p>
             </div>
-           
         </div>
         </div>
     </div>
