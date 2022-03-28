ad85246ef60c344777580cb03cb3cffb92588fc8
unknown <tamiloreolaniyi@gmail.com>
2022-03-28 21:14:33 +0100

    hero image

---
+++ src/components/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='hero'>
+        <div class="card bg-dark text-white border-0">
+        <img src="./assets/bg3.jpg" class="card-img" alt="Background" height='550px'/>
+        <div class="card-img-overlay">
+            <div className="container">
+            <h5 class="card-title display-3">NEW SEASON ARRIVAL</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+           
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
