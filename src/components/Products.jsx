554dd088837c212e47b9558cef343a0d8124d705
unknown <tamiloreolaniyi@gmail.com>
2022-03-28 21:36:55 +0100

    useState product component

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,6 +1,15 @@
 import React, {useState, useEffect} from 'react'
 
 function Products() {
+    const [data, setData] = useState([]);
+    const [filter,setFilter] = useState(data);
+    const [loading, setLoading] = useState(false);
+
+    let componentMounted = true;
+
+    useEffect(() => {
+
+    },[input]) 
   return (
     <div>
 
