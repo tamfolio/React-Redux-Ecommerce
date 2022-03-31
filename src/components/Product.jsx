f2c275da756903a912298dc8768462c04a860e94
unknown <tamiloreolaniyi@gmail.com>
2022-03-31 15:50:35 +0100

    product page

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,5 +1,8 @@
 import React,{useState, useEffect} from 'react'
 import { useParams } from 'react-router'
+import { NavLink } from 'react-router-dom';
+import Skeleton from 'react-loading-skeleton';
+
 
 function Product() {
     const { id } = useParams();
@@ -19,7 +22,18 @@ function Product() {
     const Loading = () => {
         return (
             <>
-                Loading...
+              <div className="col-md-6">
+                  <Skeleton height={400}/>
+              </div>
+              <div className="col-md-6" style={{lineHeight: 2}}>
+                  <Skeleton height={50} width={300}/>
+                  <Skeleton height={75}/>
+                  <Skeleton height={25} width={150}/>
+                  <Skeleton height={50}/>
+                  <Skeleton height={150}/>
+                  <Skeleton height={50} width={100}/>
+                  <Skeleton height={50} width={100} style={{marginLeft: 6}}/>
+              </div>
             </>
         )
     }
@@ -27,15 +41,40 @@ function Product() {
     const ShowProduct = () => {
         return (
             <>
-                 
+               <div className="col-md-6">
+                    <img src={product.image} alt={product.title}
+                    height='400px' width='400px'/>
+                </div>
+                <div className="col-md-6">
+                <h4 className="text-uppercase text-black-50">
+                    {product.category}
+                </h4>
+                <h1 className='display-5'>{product.title}</h1>
+                <p className="lead">
+                        Rating {product.rating && product.rating.rate}
+                        <i className="fa fa-star"></i>
+                </p>
+                <h3 className='display-6 fw-bold my-4'>
+                    ${product.price}
+                </h3>
+                <p className="lead">
+                    {product.description}
+                </p>
+                <button className="btn btn-outline-dark px-4 py-2">
+                    Add to Cart
+                </button>
+                <NavLink to='/cart' className="btn btn-outline-dark ms-2 px-3 py-2">
+                    Go to Cart
+                </NavLink>
+                </div>  
             </>
         )
     }
 
   return (
     <div>
-        <div className="container">
-            <div className="row">
+        <div className="container py-5">
+            <div className="row py-5">
                 {loading ? <Loading/> : <ShowProduct/> }
             </div>
         </div>
